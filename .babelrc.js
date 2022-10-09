1f0e7abfcbfac01ff7b7b05998db2fdd6cbbb80f
Bong Nguyen <vanbong@gmail.com>
2022-10-09 13:01:03 +1100

    Add build script to transpile code

--- .babelrc.js
+++ .babelrc.js
@@ -11,7 +11,7 @@ module.exports = {
   ],
   presets: [
     ["@babel/preset-env", {
-      targets: {electron: process.versions.electron || process.env.ELECTRON_VERSION}
+      targets: {electron: process.versions.electron || process.env.ELECTRON_VERSION || "12.2.3"}
     }],
     "@babel/preset-react"
   ],
